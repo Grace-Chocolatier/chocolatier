5ca83ea4768fa1e1928cee1becf9fc59c2b3d99d
Jeffrey Cheung <jeffreyy.cheungg@gmail.com>
2018-01-09 16:50:15 -0500

    first try on associations with single product view

--- server/api/products.js
+++ server/api/products.js
@@ -3,13 +3,13 @@ const {Product} = require('../db/models')
 module.exports = router
 
 router.get('/', (req, res, next) => {
-	Product.findAll()
+	Product.findAll({ include: [{ all: true }]})
 	.then(products => res.json(products))
 	.catch(next)
 })
 
 router.get('/:id', (req, res, next) => {
-	Product.findOne({where: {id: req.params.id}})
+	Product.findOne({where: {id: req.params.id}, include: [{all: true}]})
 	.then(product => res.json(product))
 	.catch(next)
 })
