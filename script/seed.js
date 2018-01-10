ab70f45432c2fee5f2ff43139023372d54aef403
Tim LaTorre <timlatorre@gmail.com>
2018-01-10 11:34:41 -0500

    user model: added email validation; product model: added inventory colum; updated seed file w/ inventory

--- script/seed.js
+++ script/seed.js
@@ -28,24 +28,24 @@ async function seed () {
   console.log(`seeded successfully`)
 
   const products = await Promise.all([
-    Product.create({name: 'Butterscotch Square', imageUrl: 'item_ButterscotchSquare.jpg', category: 'individual', price: 3.5, description: 'A soft center of grained brown sugar and cream with a touch of vanilla covered in milk chocolate.'}),
-    Product.create({name: 'Cafe au Lait Truffle', imageUrl: 'item_CafeauLaitTruffle.jpg', category: 'individual', price: 3.5, description: 'Smooth, rich truffle center of white chocolate, cream and coffee covered in rich dark chocolate and decorated with milk chocolate lace.'}),
-    Product.create({name: 'Chelsea', imageUrl: 'item_Chelsea.jpg', category: 'individual', price: 3.5, description: 'Creamy soft center with chocolate liquor and pecans covered in smooth milk chocolate.'}),
-    Product.create({name: 'Mocha', imageUrl: 'item_Mocha.jpg', category: 'individual', price: 3.5, description: 'Creamy center flavored with coffee and cream covered in smooth milk chocolate and decorated with chocolate rice.'}),
-    Product.create({name: 'Normandie', imageUrl: 'item_Normandie.jpg', category: 'individual', price: 3.5, description: 'Creamy vanilla brown sugar soft center with English walnuts and almonds, coated in rich dark chocolate.'}),
-    Product.create({name: 'Scotchmallow', imageUrl: 'item_Scotchmallow.jpg', category: 'individual', price: 3.5, description: 'A layer of caramel with a layer of honey marshmallow covered in rich, dark chocolate.'}),
-    Product.create({name: 'Akosombo Bar 68%', imageUrl: 'bar_Acosombo.jpg', category: 'bar', price: 7, description: 'Intense dark chocolate with notes of roasted coffee.'}),
-    Product.create({name: 'Coro Bar 100%', imageUrl: 'bar_Coro.jpg', category: 'bar', price: 7, description: 'Pure cocoa, this bar is the most intense.'}),
-    Product.create({name: 'Cuana Bar 74%', imageUrl: 'bar_Cuana.jpg', category: 'bar', price: 7, description: 'Robust pure dark chocolate.'}),
-    Product.create({name: 'Marao Bar 60%', imageUrl: 'bar_Marao.jpg', category: 'bar', price: 7, description: 'Dark chocolate with bursts of roasted almond.'}),
-    Product.create({name: 'Monsera Bar 37%', imageUrl: 'bar_Monsera.jpg', category: 'bar', price: 7, description: 'Milk chocolate with pronounced notes of cacao.'}),
-    Product.create({name: 'Orinoco Bar 60%', imageUrl: 'bar_Orinoco.jpg', category: 'bar', price: 7, description: 'Floral and full-bodied dark chocolate.'}),
-    Product.create({name: 'Tolima Bar 37%', imageUrl: 'bar_Tolima.jpg', category: 'bar', price: 7, description: 'Milk chocolate with crispy cereals.'}),
-    Product.create({name: 'Gesture Gift Box, 14 pieces ', imageUrl: 'box_14pc.jpg', category: 'box', price: 30, description: 'Irresistible Miniature Gift Boxes.'}),
-    Product.create({name: 'Assorted Box, 29 pieces', imageUrl: 'box_29pc.jpg', category: 'box', price: 65, description: 'A selection of ganaches and pralinés in dark and milk chocolate.'}),
-    Product.create({name: 'Assorted Box, 40 pieces', imageUrl: 'box_40pc.jpg', category: 'box', price: 85, description: 'A selection of ganaches and pralinés in dark and milk chocolate.'}),
-    Product.create({name: 'Assorted Box, 63 pieces', imageUrl: 'box_63pc.jpg', category: 'box', price: 130, description: 'A selection of ganaches and pralinés in dark and milk chocolate.'}),
-    Product.create({name: 'Assorted Box, 103 pieces', imageUrl: 'box_103pc.jpg', category: 'box', price: 215, description: 'A selection of ganaches and pralinés in dark and milk chocolate.'})
+    Product.create({name: 'Butterscotch Square', imageUrl: 'item_ButterscotchSquare.jpg', category: 'individual', price: 3.5, description: 'A soft center of grained brown sugar and cream with a touch of vanilla covered in milk chocolate.', inventory: 186}),
+    Product.create({name: 'Cafe au Lait Truffle', imageUrl: 'item_CafeauLaitTruffle.jpg', category: 'individual', price: 3.5, description: 'Smooth, rich truffle center of white chocolate, cream and coffee covered in rich dark chocolate and decorated with milk chocolate lace.', inventory: 254}),
+    Product.create({name: 'Chelsea', imageUrl: 'item_Chelsea.jpg', category: 'individual', price: 3.5, description: 'Creamy soft center with chocolate liquor and pecans covered in smooth milk chocolate.', inventory: 385}),
+    Product.create({name: 'Mocha', imageUrl: 'item_Mocha.jpg', category: 'individual', price: 3.5, description: 'Creamy center flavored with coffee and cream covered in smooth milk chocolate and decorated with chocolate rice.', inventory: 264}),
+    Product.create({name: 'Normandie', imageUrl: 'item_Normandie.jpg', category: 'individual', price: 3.5, description: 'Creamy vanilla brown sugar soft center with English walnuts and almonds, coated in rich dark chocolate.', inventory: 263}),
+    Product.create({name: 'Scotchmallow', imageUrl: 'item_Scotchmallow.jpg', category: 'individual', price: 3.5, description: 'A layer of caramel with a layer of honey marshmallow covered in rich, dark chocolate.', inventory: 211}),
+    Product.create({name: 'Akosombo Bar 68%', imageUrl: 'bar_Acosombo.jpg', category: 'bar', price: 7, description: 'Intense dark chocolate with notes of roasted coffee.', inventory: 86}),
+    Product.create({name: 'Coro Bar 100%', imageUrl: 'bar_Coro.jpg', category: 'bar', price: 7, description: 'Pure cocoa, this bar is the most intense.', inventory: 101}),
+    Product.create({name: 'Cuana Bar 74%', imageUrl: 'bar_Cuana.jpg', category: 'bar', price: 7, description: 'Robust pure dark chocolate.', inventory: 56}),
+    Product.create({name: 'Marao Bar 60%', imageUrl: 'bar_Marao.jpg', category: 'bar', price: 7, description: 'Dark chocolate with bursts of roasted almond.', inventory: 62}),
+    Product.create({name: 'Monsera Bar 37%', imageUrl: 'bar_Monsera.jpg', category: 'bar', price: 7, description: 'Milk chocolate with pronounced notes of cacao.', inventory: 73}),
+    Product.create({name: 'Orinoco Bar 60%', imageUrl: 'bar_Orinoco.jpg', category: 'bar', price: 7, description: 'Floral and full-bodied dark chocolate.', inventory: 94}),
+    Product.create({name: 'Tolima Bar 37%', imageUrl: 'bar_Tolima.jpg', category: 'bar', price: 7, description: 'Milk chocolate with crispy cereals.', inventory: 35}),
+    Product.create({name: 'Gesture Gift Box, 14 pieces ', imageUrl: 'box_14pc.jpg', category: 'box', price: 30, description: 'Irresistible Miniature Gift Boxes.', inventory: 34}),
+    Product.create({name: 'Assorted Box, 29 pieces', imageUrl: 'box_29pc.jpg', category: 'box', price: 65, description: 'A selection of ganaches and pralinés in dark and milk chocolate.', inventory: 46}),
+    Product.create({name: 'Assorted Box, 40 pieces', imageUrl: 'box_40pc.jpg', category: 'box', price: 85, description: 'A selection of ganaches and pralinés in dark and milk chocolate.', inventory: 24}),
+    Product.create({name: 'Assorted Box, 63 pieces', imageUrl: 'box_63pc.jpg', category: 'box', price: 130, description: 'A selection of ganaches and pralinés in dark and milk chocolate.', inventory: 14}),
+    Product.create({name: 'Assorted Box, 103 pieces', imageUrl: 'box_103pc.jpg', category: 'box', price: 215, description: 'A selection of ganaches and pralinés in dark and milk chocolate.', inventory: 8})
   ])
   console.log(`seeded ${products.length} products`)
   console.log(`seeded successfully`)
