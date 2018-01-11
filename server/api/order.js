51986e3ae497dde831f905c3c7f9dc5cd33c62e4
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2018-01-11 10:32:08 -0500

    fixes unauthenticated user confirm order functionality

--- server/api/order.js
+++ server/api/order.js
@@ -6,6 +6,9 @@ router.post('/:id', (req, res, next) => {
   if (req.user && Number(req.user.id) === Number(req.params.id)) {
     Order.createOrder(Number(req.params.id), req.body)
     res.sendStatus(204)
+  } else if (!req.user) {
+    Order.createOrder(null, req.body)
+    res.sendStatus(204)
   } else {
     let err = new Error(404);
     next(err)
