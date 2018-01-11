4f3d1b20d3e6e7673d81c2ac0abfc10d606af84c
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2018-01-11 00:20:18 -0500

    fixes client side error message on confirming order

--- server/api/order.js
+++ server/api/order.js
@@ -3,12 +3,7 @@ const {Order, Order_item} = require('../db/models')
 module.exports = router;
 
 router.post('/:id', (req, res, next) => {
-  if (req.user && Number(req.user.id) === Number(req.params.id)) {
-    console.log('made it through security auth', req.body)
-    Order.createOrder(Number(req.params.id), req.body)
-    .then(receipt => console.log("receipt is", receipt))
-    .catch(next)
-  }
-  next()
+  if (req.user && Number(req.user.id) === Number(req.params.id)) Order.createOrder(Number(req.params.id), req.body)
+  res.sendStatus(204)
 });
 
