61947ec591a7455e7625876c834fabcd5af8bea4
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2018-01-11 13:55:08 -0500

    makes single product buttons look nicer

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -31,13 +31,13 @@ class SingleProduct extends Component {
               Quantity:
                 <input onChange={this.props.handleChange.bind(this)} type="text" value={this.state.quantity} name="name" />
             </label>
-            <div className="listViewButtons">
-              <RaisedButton className="raised_button" label="Add To Cart" disabled={!this.state.quantity > 0} onClick={e => this.props.handleSubmit.call(this, e, this.props.product, this.state.quantity)} style={style} />
+        </form>
+        <h1>{product.name}</h1>
+        <div className="listViewButtons">
+              <RaisedButton label="Add To Cart" disabled={!this.state.quantity > 0} onClick={e => this.props.handleSubmit.call(this, e, this.props.product, this.state.quantity)} style={style} />
               <RaisedButton className="raised_button" label="+" onClick={this.props.incrementQuantity.bind(this)} style={style} />
               <RaisedButton className="raised_button" label="-" disabled={!this.state.quantity > 0} style={style} onClick={this.props.decrementQuantity.bind(this)} />
-            </div>
-          </form>
-        <h1>{product.name}</h1>
+        </div>
       </div>
     );
   }
