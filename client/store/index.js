5e391d57bd47f7c8956c293b2760beb9ed8867d6
Jeffrey Cheung <jeffreyy.cheungg@gmail.com>
2018-01-15 09:43:19 -0500

    prepping for merge with admin-console

--- client/store/index.js
+++ client/store/index.js
@@ -5,11 +5,13 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import user from './user'
 import products from './products'
 import product from './product'
-import cart from './cart'
-import categories from './categories'
+import cart from './cart';
+import users from './users'
+import orders from './orders'
+import singleOrder from './order'
 
 
-const reducer = combineReducers({user, products, product, cart, categories})
+const reducer = combineReducers({user, products, product, cart, users, orders, singleOrder})
 const middleware = composeWithDevTools(applyMiddleware(
   thunkMiddleware,
   createLogger({collapsed: true})
@@ -19,3 +21,6 @@ const store = createStore(reducer, middleware)
 export default store
 export * from './user'
 export * from './products'
+export * from './users'
+export * from './orders'
+export * from './order'
