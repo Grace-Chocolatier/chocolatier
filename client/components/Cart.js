ff027bf82828b56e38d588f03d74e97a2069025d
Jeffrey Cheung <jeffreyy.cheungg@gmail.com>
2018-01-16 12:41:51 -0500

    remove from cart functionality

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Route, Switch, Link, withRouter } from 'react-router-dom';
-import { fetchCart, clearCart } from '../store/cart';
+import { fetchCart, clearCart, deleteItem } from '../store/cart';
 import orderUtils from '../../utils/orderUtils';
 import {
   Table,
@@ -16,58 +16,65 @@ import RaisedButton from 'material-ui/RaisedButton';
 
 const style = {
   margin: 12,
+  textAlign: 'center'
 };
 
 class Cart extends Component {
-  constructor(props){
-    super(props);
-    this.state = {madeOrder: false}
-  }
+	constructor(props){
+		super(props);
+		this.state = {
+			madeOrder: false
+		}
+	}
 
 	render() {
 		return (
-      <div>
-       { this.state.madeOrder ?
-      	<h3 className='subtext'>Order Successful!</h3> :
-				<h3 className='subtext'>Confirm Your Order</h3> }
-
-				<Table>
-					<TableHeader>
-						<TableRow>
-							<TableHeaderColumn> </TableHeaderColumn>
-							<TableHeaderColumn>Name</TableHeaderColumn>
-							<TableHeaderColumn>Price</TableHeaderColumn>
-							<TableHeaderColumn>Quantity</TableHeaderColumn>
-						</TableRow>
-					</TableHeader>
-
-					<TableBody>
-						{this.props.cart.map((product) => (
-							<TableRow key={product.id}>
-								<TableRowColumn><img src={product.imgUrl} /></TableRowColumn>
-								<TableRowColumn><Link to={`/products/${product.id}`}>{product.name}</Link></TableRowColumn>
-								<TableRowColumn>${product.price}</TableRowColumn>
-								<TableRowColumn>{product.quantity}</TableRowColumn>
+	      <div>
+	       { this.state.madeOrder ?
+	      	<h3 className='subtext'>Order Successful!</h3> :
+					<h3 className='subtext'>Confirm Your Order</h3> }
+					<Table>
+						<TableHeader adjustForCheckbox={false} displaySelectAll={false}>
+							<TableRow>
+								<TableHeaderColumn style={style}>Name</TableHeaderColumn>
+								<TableHeaderColumn style={style}>Price</TableHeaderColumn>
+								<TableHeaderColumn style={style}>Quantity</TableHeaderColumn>
+								<TableHeaderColumn style={style}>Delete/Edit</TableHeaderColumn>
 							</TableRow>
-						))}
-					</TableBody>
-				</Table>
+						</TableHeader>
 
-				<RaisedButton
-					label="Confirm Order"
-					style={style}
-					onClick={event => this.props.onConfirm.call(this, event, this.props.user.id, this.props.cart)}
-					disabled={!this.props.cart.length > 0} />
+						<TableBody displayRowCheckbox={false}>
+							{this.props.cart.map((product) => (
+								<TableRow key={product.id}>
+									<TableRowColumn style={style}><Link to={`/products/${product.id}`}>{product.name}</Link></TableRowColumn>
+									<TableRowColumn style={style}>${product.price}</TableRowColumn>
+									<TableRowColumn style={style}>{product.quantity}</TableRowColumn>
+									<TableRowColumn style={style}>
+										<RaisedButton 
+											label="Delete"
+											onClick={(event) => this.props.onDelete.call(this, event, product.id)} />
+										<RaisedButton label="Edit" style={style} />
+									</TableRowColumn>
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
 
-				<RaisedButton
-					label="Clear Cart"
-					style={style}
-					onClick={this.props.onClear}
-					disabled={!this.props.cart.length > 0} />
-			</div>
-		)
+					<RaisedButton
+						label="Confirm Order"
+						style={style}
+						onClick={event => this.props.onConfirm.call(this, event, this.props.user.id, this.props.cart)}
+						disabled={!this.props.cart.length > 0} />
+
+					<RaisedButton
+						label="Clear Cart"
+						style={style}
+						onClick={this.props.onClear}
+						disabled={!this.props.cart.length > 0} />
+				</div>
+			)
+		}
 	}
-}
 
 function mapStateToProps(state) {
 	return {
@@ -80,14 +87,18 @@ function mapDispatchToProps(dispatch) {
 	return {
 		onConfirm: function (event, userId, cart) {
 			event.preventDefault();
-      orderUtils.makeOrder(userId, cart);
-      dispatch(clearCart())
-      this.setState({madeOrder: true})
-    },
-    onClear: function (event) {
-      event.preventDefault();
-      dispatch(clearCart())
-    }
+			orderUtils.makeOrder(userId, cart);
+			dispatch(clearCart())
+			this.setState({madeOrder: true})
+		},
+		onClear: function (event) {
+			event.preventDefault();
+			dispatch(clearCart())
+		},
+		onDelete: function(event, productId) {
+			event.preventDefault();
+			dispatch(deleteItem(productId))
+		}
 	}
 }
 
