8a94a6f4765a9ee63eab5f2b99f24d5f0c3c8acd
Elizabeth Farrier <elizabeth_farrier@alumni.brown.edu>
2018-01-16 17:15:38 -0500

    part two

--- server/api/products.js
+++ server/api/products.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const {Product} = require('../db/models')
+const {Product, Review} = require('../db/models')
 module.exports = router
 
 router.get('/', (req, res, next) => {
@@ -8,15 +8,17 @@ router.get('/', (req, res, next) => {
 	.catch(next)
 })
 
+router.post('/:id/reviews', (req, res, next) => {
+	Review.create(req.body)
+	.then(newReview => {
+		console.log('***review***', newReview)
+		res.json(newReview)
+	})
+	.catch(next)
+})
+
 router.get('/:id', (req, res, next) => {
 	Product.findOne({where: {id: req.params.id}, include: [{all: true}]})
 	.then(product => res.json(product))
 	.catch(next)
-})
-
-router.put('/:id', (req, res, next) => {
-  Product.update(req.body, { where: {id: req.params.id} })
-  .then(() => Product.findById(Number(req.params.id)))
-  .then(product => res.json(product))
-  .catch(err => console.error(err));
 })
